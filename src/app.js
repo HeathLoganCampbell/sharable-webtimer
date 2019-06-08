c8f10ad588fcfff8d8790c5a20025f2435e716c8
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-08 17:49:20 +1200

    Fix Babel build problems

--- src/app.js
+++ src/app.js
@@ -5,19 +5,18 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/../views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/../public")));
 
 app.use("/", indexRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import * as express from "express";
+import express from "express";
 import TimeController from "../controllers/time.controller";
 var router = express.Router();
 
