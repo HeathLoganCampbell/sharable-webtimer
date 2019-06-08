f7d6c25c7b12a63ba0ef571fd0ad10c83c68d24f
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-08 17:08:06 +1200

    change to babel

---
+++ controllers/time.controller.js
@@ -0,0 +1,5 @@
+export default class TimeController {
+	static interpretTime(timeStr) {
+		console.log("Called");
+	}
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+import * as express from "express";
+import TimeController from "../controllers/time.controller";
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+	console.log(req.params);
+	res.render("index", { title: "Express" });
+});
+
+router.get("/*", function(req, res, next) {
+	TimeController.interpretTime(req.params[0]);
+	res.render("index", { title: req.params });
 });
 
 module.exports = router;
