05952247d67df8d71d2af42b06063ec957a08e4b
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-09 21:43:52 +1200

    render counter decrease

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,23 @@
 import express from "express";
-import TimeController from "../controllers/time.controller";
+import TimeController from "../parser/time";
 var router = express.Router();
 
 /* GET home page. */
-router.get(/^\/(\d?\d[ymwdhms]+)$/, function (req, res, next) {
-	console.log(req.params);
-	res.render("index", { title: "Express" });
+router.get(/^\/(\d?\d[ywdhms]+)$/, function (req, res, next) {
+	//[ywdhms]
+	//y w d h m s
+
+	// TimeController
+
+	res.render("index", { title: "timer", seconds: "1000" });
 });
 
-router.get("/", function (req, res, next) {
-	TimeController.interpretTime(req.params[0]);
-	res.render("index", { title: req.params });
-});
+// router.get("/", function (req, res, next) {
+// 	TimeController.interpretTime(req.params[0]);
+// 	res.render("index", { title: "welp  fuck" });
+// });
+//
+//The user types "./4h20m"
+//the site starts couting down from there
 
 module.exports = router;
