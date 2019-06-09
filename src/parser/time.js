0922c9b7cbc790198d37b61ddb59ae23e089a637
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-10 00:10:52 +1200

    add home page

--- src/parser/time.js
+++ src/parser/time.js
@@ -1,6 +1,9 @@
 export default class TimeController {
 	static interpretTime(timeStr) {
 		//Years, Weeks, Days, hours, minutes, seconds
+
+
+
 		console.log("Called");
 	}
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,67 @@ import express from "express";
 import TimeController from "../parser/time";
 var router = express.Router();
 
+var timeType = [
+	{
+		name: "year",
+		symbol: "y",
+		toSeconds: 31536000
+	},
+	{
+		name: "week",
+		symbol: "w",
+		toSeconds: 604800
+	},
+	{
+		name: "day",
+		symbol: "d",
+		toSeconds: 86400
+	},
+	{
+		name: "hour",
+		symbol: "h",
+		toSeconds: 3600
+	},
+	{
+		name: "minutes",
+		symbol: "m",
+		toSeconds: 60
+	},
+];
+
 /* GET home page. */
 router.get(/^\/(\d?\d[ywdhms]+)$/, function (req, res, next) {
 	//[ywdhms]
 	//y w d h m s
 
+	var seconds = 0;
+	for (var i = 0; i < timeType.length; i++) {
+		var type = timeType[i];
+		// if(req.params[0])
+		// seconds += type.toSeconds * 10;
+	}
+
+	console.log(req.params[0])
 	// TimeController
 
-	res.render("index", { title: "timer", seconds: "1000" });
+	res.render("index", { title: "timer", seconds: seconds });
+});
+
+router.get("/", function (req, res, next) {
+	//[ywdhms]
+	//y w d h m s
+
+	var seconds = 0;
+	for (var i = 0; i < timeType.length; i++) {
+		var type = timeType[i];
+		// if(req.params[0])
+		// seconds += type.toSeconds * 10;
+	}
+
+	console.log(req.params[0])
+	// TimeController
+
+	res.render("home");
 });
 
 // router.get("/", function (req, res, next) {
