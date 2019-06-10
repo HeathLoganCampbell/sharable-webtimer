f800f44c4b7c2a0a986b9fbfef8d74ddf266b679
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-06-10 23:51:42 +1200

    add 404 page & make examples external

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+	res.render("404");
 });
 
 // error handler
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,6 @@ import express from "express";
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-
-	console.log(req.params[0])
-
 	res.render("index");
 });
 
